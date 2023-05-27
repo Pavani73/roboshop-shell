a7093abd17a73e2243acc65f12b0c7b3e0169441
Pavani <pavani.chitla@gmail.com>
2023-05-27 23:43:29 +1000

    MongoDb repository

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,9 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 echo -e "\e[33mInstall mongodb server\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 
+echo -e "\e[33mUpdate Mongodb Listen Address\e[0m"
+sed -i 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+
 #modify the mongodb config file
 echo -e "\e[33mStart mongodb serevr\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
